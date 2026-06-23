5942ee108450ae6fc5c231d29c8fc59b61cebdc3
princess1hacking <victoria.mocanu@isb.ro>
2026-06-23 15:16:32 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const fs = require("fs");
 
-const app = express();
+
 app.use(express.static(_dirname));
 
 // ─── Config ───────────────────────────────────────────────────────────────────
